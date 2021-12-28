4786662646c82a0def43b1d1856021b3d975ca44
Tiến Huỳnh Thanh Tiến <htti@netcompany.com>
2021-12-28 13:10:38 +0700

    hoang test

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,11 @@
 <template>
-  <div>
-    Hello
-    <Nuxt />
+  <div class="home">
+      <div class="header">
+        
+      </div>
+      <div class="logo">
+        <img src="../assets/imgs/logo.jpg" alt="">
+      </div>
   </div>
   
 </template>
@@ -13,3 +17,29 @@ export default Vue.extend({
   name: 'IndexPage',
 })
 </script>
+
+<style lang="scss" scoped>
+.home {
+  position: relative;
+  .header {
+    background-color: #c7a67a;
+    height: 100px;
+  }
+  .logo {
+    top: 0;
+    left: 50%;
+    z-index: 10;
+    position: absolute;
+    padding: 0px;
+    margin-left: -100px;
+    
+    img {
+      width:100%;
+      height:100%;
+      object-fit: cover;
+    }
+    
+  }
+}
+
+</style>
